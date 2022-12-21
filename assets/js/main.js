84497595bc32f59bb66053e4f6ef02663ef2a1a4
khalid fifel <fifle.khalid99@gmail.com>
2022-12-21 17:20:19 +0100

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,12 @@
 let username = ""
-
-if(window.location.href === "http://localhost:63342/quize-js/quiz.html"){
+let index = 0;
+function countDown(){
     for (let i = 5; i >= 0; i--) {
         setTimeout(() =>{
             document.getElementById("counter-to-start").innerText = `${i}`
             if(i===0){
                 document.getElementById("counter-to-start").innerText = `let's Go`
-                ajax();
+                quizStart()
             }
         }, (5 - i) * 1000);
     }
@@ -17,26 +17,60 @@ function Start(){
     if(username === ""){
         document.getElementById('alert').innerText = "Please username is required";
     }else{
-        window.location.href = "./quiz.html"
+        document.querySelector(".container").innerHTML = `
+            <article>
+                <h1>Quiz well start in <br> <span id="counter-to-start">5</span></h1>
+            </article>
+        `;
+        countDown();
     }
 }
+//   ------------------------------------------------------------------------------------
 
-function ajax(){
- let ajax = new XMLHttpRequest()
-
+    let ajax = new XMLHttpRequest()
+    let data = [];
     ajax.onreadystatechange = function() {
         if ( ajax.readyState === 4 && ajax.status === 200) {
-            getData(JSON.parse(ajax.responseText));
+            res = JSON.parse(ajax.responseText);
+            res.sort(()=> Math.random() - 0.5 );
+            getData(res)
         }
     };
     ajax.open("GET", 'assets/js/typescript/data/Question.json', true);
     ajax.send();
+
+
+//   ---------------------------------------------------------------------------------------
+
+function getData(res){
+    data = res;
 }
 
-function getData(data){
-    quizStart(data)
+function nextQuestion() {
+    console.log(data[index]);
+    const container = document.querySelector(".container");
+    let answerContainer = `
+    <div>
+        <h2>  ${data[index].question}  </h2>
+        <div class="answer">
+    `;
+    for(var i = 0; i < data[index].choices.length; i++ ){
+        answerContainer += `
+        <div class="answer-items">
+            <label for="choice1">${data[index].choices[i]}</label>
+            <input type="radio" id="choice${i}" name="choice" value="i">
+        </div> 
+        `;
+    }
+
+    answerContainer += `</div></div>`;
+    container.innerHTML = answerContainer;
 }
 
 function quizStart(){
+    nextQuestion()
+}
 
+function showQuestions(){
+    quizStart()
 }
