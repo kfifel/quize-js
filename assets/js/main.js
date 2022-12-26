ac56efd2177f98437d000c12f2f3497cac8dbfa8
khalid fifel <fifle.khalid99@gmail.com>
2022-12-26 12:19:17 +0100

    add ranked component function and dashboard ons also the stepper

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,63 @@
-let username = ""
-let index = 0;
-let score = 0;
+const container = document.querySelector(".container")
+const counter_question = document.querySelector(".counter-question")
+let username;
+let index;
+let score;
+function rankedComponent(){
+    let rankedData = new Map();
+    for (let key of Object.keys(localStorage)) {
+        let value = JSON.parse(localStorage.getItem(key));
+        rankedData.set(key, value);
+    }
 
-function countDown(){
+    let content = `
+                    <article>
+                        <h1>Global Ranked</h1>
+                        <div class="ranked-users">
+                            <table>
+                            <thead>
+                                <tr>
+                                    <th>username</th>
+                                    <th>score</th>
+                                </tr>
+                            </thead>
+                            </tbody>`;
+
+                for (let [key, value] of rankedData){
+                    content += `               
+                                 <tr>
+                                    <td>${key}</td> 
+                                    <td>${value}</td> 
+                                </tr>           
+                    `;
+                }
+    content +=`</tbody>
+                             </table>
+                         </div>
+                    </article>
+    `;
+
+
+    container.innerHTML = content;
+}
+function dashboardComponent(){
+    container.innerHTML = `
+                    <article>
+                        <h1>Welcome to quiz in PHP</h1>
+                        <p>
+                            this website well offer to you a quiz of POO in PHP <br>
+                            Are you ready !!!
+                        </p>
+                    </article>
+                    <section>
+                        <label for="username" class="">Put your UserName here to starts: <br></label>
+                        <input type="text" id="username" placeholder="username ...">
+                        <button  class="start" onclick="Start()">  Let's start  </button>
+                        <div id="alert-danger"></div>
+                    </section>
+    `;
+}
+function countDownToStart(){
     quizStart();
     /*for (let i = 5; i >= 0; i--) {
         setTimeout(() =>{
@@ -16,27 +71,31 @@ function countDown(){
 }
 
 function Start(){
+    index = 0;
+    username = "";
+    score = 0;
     username = document.getElementById("username").value;
     if(username !== ""){
-        document.getElementById('alert').innerText = "Please username is required";
+        document.getElementById('alert-danger').innerText = "Please username is required";
     }else{
         document.querySelector(".container").innerHTML = `
             <article>
                 <h1>Quiz well start in <br> <span id="counter-to-start"></span></h1>
             </article>
         `;
-        countDown();
+        countDownToStart();
     }
 }
 //   ------------------------------------------------------------------------------------
 
     let ajax = new XMLHttpRequest()
-    let data = [];
+    let questions = [];
+    let size = 0;
     ajax.onreadystatechange = function() {
         if ( ajax.readyState === 4 && ajax.status === 200) {
-            res = JSON.parse(ajax.responseText);
-            res.sort(()=> Math.random() - 0.5 );
-            getData(res)
+            questions = JSON.parse(ajax.responseText);
+            size = questions.length;
+            questions.sort(()=> Math.random() - 0.5 );
         }
     };
     ajax.open("GET", 'assets/js/typescript/data/Question.json', true);
@@ -45,58 +104,83 @@ function Start(){
 
 //   ---------------------------------------------------------------------------------------
 
-function getData(res){
-    data = res;
+
+function countDownTimeQuestion() {
+    let i = 30;
+    while (i){
+        setTimeout(()=>{
+            counter_question.innerText = `${i}`;
+        }, (30 - i) * 1000);
+        i--;
+    }
 }
 
 function nextQuestion() {
-    console.log(data[index]);
-    let typeInput = data[index].answers.length > 1 ? "checkbox" : "radio"
-    const container = document.querySelector(".container");
-    let answerContainer = `
-    <div class="quiz-container">
-        <h2>  ${data[index].question}  </h2>
-        <div class="answer">
-    `;
-    for(var i = 0; i < data[index].choices.length; i++ ){
-        answerContainer += `
-            <label for="${i+1}" id="choice${i+1}" class="answer-items">
-                ${data[index].choices[i]}
-                <input type="${typeInput}" id="${i+1}" name="choice" value="${i+1}" onclick="saveAnswer('choice'+${i+1})">
-            </label>
+    if(index < size){
+        let data = questions;
+        let typeInput = data[index].answers.length > 1 ? "checkbox" : "radio"
+        let answerContainer = `
+                <div class="quiz-container">
+                    <h2>  
+                        ${data[index].question}  
+                        <div class="timer-question">
+                            <box-icon name='timer' ></box-icon>
+                            <span id="counter-question">30</span>
+                        </div>
+                    </h2>
+                    <div class="answer">
         `;
+        for(let i = 0; i < data[index].choices.length; i++ ){
+            answerContainer += `
+                <label for="${i}" id="choice${i}" class="answer-items">
+                    ${data[index].choices[i]}
+                    <input type="${typeInput}" id="${i}" name="choice" value="${i}" onclick="stylingDivAnswer()">
+                </label>
+            `;
+        }
+
+        answerContainer += `
+            <div class="alert-danger">
+                
+            </div>
+            </div>
+        <button class="btn-next" onclick="calcAnswer([${data[index].answers}])">next</button>
+    </div>`;
+        container.innerHTML = answerContainer;
+        index++;
+        countDownTimeQuestion();
+    }else{
+        showResult();
     }
-
-    answerContainer += `
-        </div>
-    <button class="btn-next" onclick="calcAnswer(data[index].answers)">next</button>
-</div>`;
-    container.innerHTML = answerContainer;
-    index++;
 }
-
+function showResult(){
+    container.innerHTML = `<h1> congratulation ${username} your Score is : ${score}%</h1>`
+    localStorage.setItem(username,score);
+}
 function quizStart(){
     nextQuestion()
 }
 
-function showQuestions(){
-    quizStart()
-}
-
-function calcAnswer(answer){
-    if(answer.length === 1){
-        let check = document.querySelector("input:checked");
-        if(answer[0] === parseInt(check.value)){
-            score += 10;
-            document.getElementById("choice"+answer.id).classList.add("valid");
-            console.log(document.getElementById("choice"+answer.id))
-            alert("teur : id = "+check.id)
+function calcAnswer(answers){
+    if(document.querySelectorAll("input:checked").length > 0){
+        document.querySelector(".btn-next").disabled = true;
+        if(answers.length === 1){
+            let check = document.querySelector("input:checked");
+            if(answers[0] === parseInt(check.value)){
+                score += 100 / size;
+            }
         }else{
-
-            alert('false');
+            let checks = document.querySelectorAll("input:checked");
+            let userChecks = [];
+            checks.forEach(check =>{
+                userChecks.push(parseInt(check.id));
+            })
+            if(answers.every((answer)=>userChecks.includes(answer)))
+                score += 100 / size;
         }
+        nextQuestion();
     }else{
-        alert('non');
+        document.querySelector(".alert-danger").innerText = "please check the correct answers";
     }
 }
 
@@ -105,8 +189,4 @@ function stylingDivAnswer() {
     let all   = document.querySelectorAll('.answer-items')
     all.forEach(e=>e.classList.remove('active'))
     check.forEach(e=>e.parentElement.classList.add('active'))
-}
-
-function saveAnswer(){
-    stylingDivAnswer();
 }
